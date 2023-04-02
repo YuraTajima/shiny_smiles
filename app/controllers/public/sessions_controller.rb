f4e886cdc0aec6809bb8f6b8cef329ff921777cc
yurapero <y1009321@gmail.com>
2023-04-02 09:46:39 +0000

    退会機能の追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,12 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_state, only: [:create]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 
   # GET /resource/sign_in
   # def new
@@ -18,7 +24,19 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  　protected
+   
+  def customer_state
+    @customer = Customer.find_by(email: params[:customer][:email])
+    
+    return if !@customer
+    
+    if @customer.valid_password?(params[:customer][:password])
+      if @customer.is_deleted == true
+        redirect_to new_customer_registration_path
+      end
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
