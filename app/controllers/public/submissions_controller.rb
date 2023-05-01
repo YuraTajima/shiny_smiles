61e6fa7a87d11b88b36e71018a2ca012dd764ad6
yurapero <y1009321@gmail.com>
2023-05-01 13:22:20 +0000

    [Add] submission

--- app/controllers/public/submissions_controller.rb
+++ app/controllers/public/submissions_controller.rb
@@ -9,19 +9,27 @@ class Public::SubmissionsController < ApplicationController
   end
 
   def show
+    @submission = Submissio.find(params[:id])
   end
 
   def edit
+    @submission = Submissio.find(params[:id])
   end
 
   def create
     @submission = Submission.new(submission_params)
-    # @submission.user_id = current_user.id
+    @submission.user_id = current_user.id
     @submission.save
     redirect_to index_submission_path
   end
 
   def update
+    @submission = Submission.find(params[:id])
+    if @submission.update(submission_params)
+      redirect_to show_submission_path(@submission.id)
+    else
+      render :show
+    end
   end
   
    private
