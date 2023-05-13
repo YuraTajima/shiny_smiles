db3444a6ba815c3121efd8f1b7beb61769a77df1
yurapero <y1009321@gmail.com>
2023-05-13 10:35:56 +0000

    [Add] abmin

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  before_action :customer_session, only: [:create]
+  before_action :customer_state, only: [:create]
   # before_action :configure_permitted_parameters, if: :devise_controller?
   
   def after_sign_in_path_for(resource)
@@ -37,6 +37,8 @@ class Public::SessionsController < Devise::SessionsController
   #     end
   #   end
   # end
+  
+  protected
   
   def customer_state
     @customer = Customer.find_by(email: params[:customer][:email])
